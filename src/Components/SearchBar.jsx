c761a90af69d05a2fa5b01f4f709520f93835cbe
SamueleRusco <119084990+SamueleRusco@users.noreply.github.com>
2023-02-18 12:52:45 +0100

    conditional doesnt work idk why

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -10,33 +10,30 @@ const HomePage = () => {
   return (
     <>
       <Row>
-        <Row>
-          <Col>
-            <Form
-              onSubmit={(e) => {
-                e.preventDefault();
-                setSearchQuery(e.target[0].value);
-              }}
-            >
-              <Form.Group className="d-flex m-5">
-                <Form.Control
-                  type="text"
-                  placeholder="Scrivi il nome della tua citta"
-                />
-                <Button className="button" type="submit">
-                  clickHere
-                </Button>
-              </Form.Group>
-            </Form>
-            {clima.main && <ClimaCard clima={clima} />}
-            <FetchCitta
-              passoUrlAFetch={searchQuery}
-              prendoRisultatoFetch={(risultatoFetch) =>
-                setClima(risultatoFetch)
-              }
-            />
-          </Col>
-        </Row>
+        <Col>
+          <Form
+            className="search-box"
+            onSubmit={(e) => {
+              e.preventDefault();
+              setSearchQuery(e.target[0].value);
+            }}
+          >
+            <Form.Group className="d-flex m-5">
+              <Form.Control
+                type="text"
+                placeholder="Scrivi il nome della tua citta"
+              />
+              <Button className="button" type="submit">
+                clickHere
+              </Button>
+            </Form.Group>
+          </Form>
+          {clima.main && <ClimaCard clima={clima} />}
+          <FetchCitta
+            passoUrlAFetch={searchQuery}
+            prendoRisultatoFetch={(risultatoFetch) => setClima(risultatoFetch)}
+          />
+        </Col>
       </Row>
     </>
   );
